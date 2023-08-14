e12cc5c7dd417d0f2ae07e9585381d6dd1ecf81b
sohanemon <m.sohanemon@gmail.com>
2023-08-15 00:17:17 +0600

    feat: navbar parallax

--- app/hero.tsx
+++ app/hero.tsx
@@ -1,6 +1,10 @@
-import { HTMLAttributes } from 'react';
+'use client';
+
+import { HTMLAttributes, useRef } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import useScrollTransform from '@/hook/scroll-transform';
+import { motion, useSpring } from 'framer-motion';
 
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
@@ -9,9 +13,33 @@ import { Typography } from '@/components/ui/typography';
 interface HeroProps extends HTMLAttributes<HTMLDivElement> {}
 
 export default function Hero({ className, ...props }: HeroProps) {
+  const target = useRef(null);
+  const y = useScrollTransform({
+    target,
+    offset: ['start start', 'end start'],
+    outputRange: [0, 500],
+  });
+  const opacity = useScrollTransform({
+    target,
+    offset: ['start start', 'end start'],
+    outputRange: [1, 0],
+  });
+  const scale = useScrollTransform({
+    target,
+    offset: ['start start', 'end start'],
+    outputRange: [1, 1.2],
+  });
+
   return (
-    <section className={cn('relative', className, {})} {...props}>
-      <div className="blue-overlay">
+    <section
+      ref={target}
+      className={cn('relative overflow-hidden', className, {})}
+    >
+      <motion.div
+        style={{ y, opacity, scale }}
+        transition={{ duration: 2, type: 'spring' }}
+        className="blues-overlay"
+      >
         <Image
           src={`https://s3-alpha-sig.figma.com/img/5cdd/fbd0/44f2660a20dd6837ff31aefdfff9cc7c?Expires=1692576000&Signature=J7QBjHCorAEyJJpKTDyJGdW1uf7ZWKJIhdnmS6zuQduWYGJFnJ3jDy0yD40Zwsq-hu8bz8q2bg-nbPBBgswKmSgLndwhQuyF-dN4p0O9RoiEQ1TpXs4462CE9lRzUZbNk1A4Yh7Mb4WqK29PEy7-9K5Te137U7py6XqfGdbeznXbFYTd4gX6vH6SZXKeWa0Wzx9jHb7NbQ6NI0UMONXY4oj79jFaexTDC5DsbY4ChL7cF34PxBCBryidl1ildplJzFn7t9i9oHdgrwCagV7lkkwb0u9ijsl-y~uJlwsMDbaH3DLTwBwxUiiQP~LJBQW3icIcbICAQN8C6s2IXUJ8sA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4`}
           alt=""
@@ -19,7 +47,7 @@ export default function Hero({ className, ...props }: HeroProps) {
           width={1024}
           height={600}
         />
-      </div>
+      </motion.div>
       <div className="absolute inset-0 m-auto h-fit w-fit">
         <div className="container max-w-screen-sm space-y-5 text-center md:max-w-screen-md">
           <Typography variant={'heading'}>
