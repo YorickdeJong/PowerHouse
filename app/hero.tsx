1a0b0889c80299a40da2cc1d156469401ff997ae
sohanemon <m.sohanemon@gmail.com>
2023-08-16 15:57:43 +0600

    fix: bg image

--- app/hero.tsx
+++ app/hero.tsx
@@ -47,7 +47,7 @@ export default function Hero({ className, ...props }: HeroProps) {
         <Image
           src={`https://s3-alpha-sig.figma.com/img/5cdd/fbd0/44f2660a20dd6837ff31aefdfff9cc7c?Expires=1692576000&Signature=J7QBjHCorAEyJJpKTDyJGdW1uf7ZWKJIhdnmS6zuQduWYGJFnJ3jDy0yD40Zwsq-hu8bz8q2bg-nbPBBgswKmSgLndwhQuyF-dN4p0O9RoiEQ1TpXs4462CE9lRzUZbNk1A4Yh7Mb4WqK29PEy7-9K5Te137U7py6XqfGdbeznXbFYTd4gX6vH6SZXKeWa0Wzx9jHb7NbQ6NI0UMONXY4oj79jFaexTDC5DsbY4ChL7cF34PxBCBryidl1ildplJzFn7t9i9oHdgrwCagV7lkkwb0u9ijsl-y~uJlwsMDbaH3DLTwBwxUiiQP~LJBQW3icIcbICAQN8C6s2IXUJ8sA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4`}
           alt=""
-          className=" max-h-screen min-h-[500px] w-full object-cover"
+          className=" max-h-screen min-h-[500px] w-full object-cover max-md:scale-[2] origin-right"
           width={1024}
           height={600}
         />
