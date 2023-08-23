09257565ce9f44ed5a642ca0e83f22756d35a73c
Yorick De Jong <yorickdejong@gmail.com>
2023-08-23 14:46:29 +0200

    optimized person

--- app/about/person.tsx
+++ app/about/person.tsx
@@ -6,16 +6,20 @@ import { useScroll, useTransform } from 'framer-motion';
 import { cn } from '@/lib/utils';
 import { Images } from '@/components/images';
 import Motion from '@/components/motion';
+import { useMediaQuery } from '@/hook/media-query';
 
 interface PersonProps extends HTMLAttributes<HTMLDivElement> {}
 
 export default function Person({ className, ...props }: PersonProps) {
+  const phone = useMediaQuery('(max-width: 768px)');
   const { scrollY } = useScroll();
   const x1 = useTransform(scrollY, [0, 1000], [0, -1000]);
   const x2 = useTransform(scrollY, [0, 1000], [0, 1000]);
   const x3 = useTransform(scrollY, [0, 1400], [0, -1400]);
   const x4 = useTransform(scrollY, [0, 1400], [0, 1400]);
   const opacity = useTransform(scrollY, [0, 600], [1, 0]);
+
+  
   return (
     <>
       <div
@@ -27,16 +31,16 @@ export default function Person({ className, ...props }: PersonProps) {
         {...props}
       >
         <Motion style={{ x: x1 }}>
-          <Images.person1 />
+          <Images.person1 width = {phone ? 600 : 1200}/>
         </Motion>
         <Motion style={{ x: x2 }}>
-          <Images.person2 />
+          <Images.person2 width = {phone ? 600 : 1200}/>
         </Motion>
         <Motion style={{ x: x3 }}>
-          <Images.person3 className="rotate-12" />
+          <Images.person3 className="rotate-12" width = {phone ? 600 : 1200}/>
         </Motion>
         <Motion style={{ x: x4 }}>
-          <Images.person4 />
+          <Images.person4 width = {phone ? 600 : 1200}/>
         </Motion>
       </div>{' '}
       {/* @ts-ignore */}
